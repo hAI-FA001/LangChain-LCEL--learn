6ea2d984327e16558417a02f6ea6d0cf0c199b17
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2024-08-14 08:20:30 +0500

    use HF

--- lcel.py
+++ lcel.py
@@ -1,7 +1,7 @@
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.messages import HumanMessage
-from langchain_huggingface import ChatHuggingFace
+from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
 
 from dotenv import load_dotenv
 
@@ -9,11 +9,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 if __name__ == "__main__":
+    model_repo = "microsoft/Phi-3-mini-4k-instruct"
     prompt_content = "Tell me a joke aboue {topic}. Be brief and concise."
     topic = "ice cream"
 
     prompt = ChatPromptTemplate.from_template(prompt_content)
-    model = ChatHuggingFace()
+    model = ChatHuggingFace(llm=HuggingFaceEndpoint(repo_id=model_repo, task="text-generation", max_new_tokens=512))
     output_parser = StrOutputParser()
 
     # compose a chain using pipes (like in terminal)
@@ -23,4 +24,4 @@ if __name__ == "__main__":
     # everything has .invoke() (they implement Runnable)
     print(f"chain.invoke(): {chain.invoke(input={"topic": topic})}")
     print(f"prompt.invoke(): {prompt.invoke(input={"topic": topic})}")
-    print(f"model.invoke(): {model.invoke(HumanMessage(content=prompt_content))}")
+    print(f"model.invoke(): {model.invoke(prompt.invoke(input={"topic": topic}))}")
